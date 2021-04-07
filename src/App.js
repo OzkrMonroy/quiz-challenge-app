f1e5cf5b324c1b0fdbf80a7c037eeb2292a7cdc1
Oscar Monroy <oscarmonroy657@gmail.com>
2021-04-07 15:31:16 -0600

    Get all the API data and create 20 questions with their answers is ready.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/card/card.component';
 import { AppContainer } from './styledComponents/app-container';
 
 function App() {
+  const [allCountries, setAllCountries] = useState(null);
+
+  useEffect(() => {
+    const getAllCountriesData = async () => {
+      try {
+        const response = await axios('https://restcountries.eu/rest/v2/region/europe');
+        const data = await response.data;
+        setAllCountries(data)
+
+        const allQuestions = []
+
+        for (let i = 0; i < 20; i++) {
+          const answer = data[Math.floor(Math.random() * data.length)];
+          const { name, capital } = answer;
+          const posibleAnswers = [];
+          const question = [];
+
+          for (let i = 0; i < 3; i++) {
+            const answer = data[Math.floor(Math.random() * data.length)];
+            const { capital } = answer;
+            posibleAnswers.push(capital)
+          }
+          posibleAnswers.push(capital)
+          const questionData = {answer : { name, capital}, posibleAnswers}
+          question.push(questionData)
+          allQuestions.push(question)
+        }
+        console.log(allQuestions);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAllCountriesData()
+  }, [])
+
   return (
     <AppContainer>
       <Card/>
